51f62471397f9e8b006bc3f83f68923994b899d8
Rodrigo Duran <rads2995@gmail.com>
2025-11-10 17:43:27 -0500

    Enough changes to write the report!

--- src/findmytau/main.py
+++ src/findmytau/main.py
@@ -4,21 +4,43 @@ import numpy as np
 from .sde import SDE
 
 def main():
-    sde: SDE = SDE()
-    sde.simulate()
-    print(sde.optimal_stopping())
+    
+    print("Pricing of American Style Put Options:")
 
-    plt.figure(figsize=(10,6))
-    for i in range(5):
-        path = sde.X[:, i]
-        plt.plot(np.linspace(0, sde.T, sde.M+1), path.get(), lw=1, alpha=0.7)
-        
-        # mark the stopping point with a red dot
-        t_stop = sde.stopping_times[i].get()
-        plt.scatter(t_stop * sde.dt, path[t_stop].get(), color="red", zorder=3)
+    prices: list[float] = [36.0, 38.0, 40.0, 42.0, 44.0]
 
-    plt.axhline(1.0, color="black", ls="--", lw=1)  # strike boundary
-    plt.title("Sample Trajectories with Optimal Stopping Decisions")
-    plt.xlabel("Time")
-    plt.ylabel("Underlying Price")
-    plt.savefig("result.png")
+    for price in prices:
+        sde: SDE = SDE(
+            N = 100000,
+            M = 50,
+            mu = 0.06,
+            sigma = 0.2,
+            x0 = price,
+            strike = 40.0,
+            T = 1.0
+        )
+        sde.simulate()
+        sde.find_optimal_stopping()
+        print(
+            "For S = {:.2f}, sigma = {:.2f}, and T = {:.2f}, the optimal stopping value is {:.12f}".format(
+                sde.x0,
+                sde.sigma,
+                sde.T,
+                sde.optimal_stopping
+            )
+        )
+
+        plt.figure(figsize=(10,6))
+        for i in range(10):
+            path = sde.X[:, i]
+            plt.plot(np.linspace(0, sde.T, sde.M+1), path.get(), lw=1, alpha=0.7)
+            
+            # mark the stopping point with a red dot
+            t_stop = sde.stopping_times[i].get()
+            plt.scatter(t_stop * sde.dt, path[t_stop].get(), color="red", zorder=3)
+
+        plt.axhline(sde.strike, color="black", ls="--", lw=1)  # strike boundary
+        plt.title("Sample Trajectories with Optimal Stopping Decisions")
+        plt.xlabel("Time")
+        plt.ylabel("Underlying Price")
+        plt.savefig("quantile_stock_price_" + str(price) + ".png")
--- src/findmytau/sde.py
+++ src/findmytau/sde.py
@@ -2,21 +2,25 @@ import cupy as cp
 import xgboost as xgb
 
 class SDE:
+
     def __init__(
         self,
         N: cp.int64 = 10000,
-        M: cp.int64 = 2000,
+        M: cp.int64 = 50,
         mu: cp.float64 = 0.05,
         sigma: cp.float64 = 0.2,
         x0: cp.float64 = 1.0,
+        strike: cp.float64 = 40.0,
         T: cp.float64 = 1.0
-    ):
+    ) -> None:
+        
         self.N: cp.int64 = N
         self.M: cp.int64 = M
-        
+
         self.mu: cp.float64 = mu
         self.sigma: cp.float64 = sigma
         self.x0: cp.float64 = x0
+        self.strike: cp.float64 = strike
 
         self.T: cp.float64 = T
         self.dt: cp.float64 = self.T / self.M
@@ -27,20 +31,22 @@ class SDE:
         self.alpha: cp.float64 = 0.5
         self.stopping_times = cp.full(self.N, self.M, dtype=cp.int64)
 
-    def simulate(self):
+        self.optimal_stopping: cp.float64 = 0.0
 
+    def simulate(self) -> None:
+        
         rng = cp.random.default_rng()
         for t in range(self.M):
             dW_X = rng.standard_normal(size=self.N, dtype=cp.float64) * cp.sqrt(self.dt)
             self.X[t + 1, :] = self.X[t, :] * cp.exp((self.mu - 0.5 * self.sigma**2) * self.dt + self.sigma * dW_X)
 
-    def optimal_stopping(self):
+    def find_optimal_stopping(self) -> None:
         
-        V: cp.float64 = cp.maximum(1.0 - self.X[-1, :], 0.0)
-        discount: cp.float64 = cp.exp(-0.05 * self.dt)
+        V: cp.float64 = cp.maximum(self.strike - self.X[-1, :], 0.0)
+        discount: cp.float64 = cp.exp(-self.mu * self.dt)
 
         for t in range(self.M - 1, -1, -1):
-            item = (1.0 - self.X[t, :]) > 0.0
+            item = (self.strike - self.X[t, :]) > 0.0   # in-the-money
             
             V = discount * V
             
@@ -50,7 +56,7 @@ class SDE:
             features = self.X[t, item].reshape(-1, 1)            
             Y = V[item]
 
-            # TODO: pass cupy arrays directly once rocm's xgboost is updated
+            # TODO: pass CuPy arrays directly once ROCm's XGBoost is updated
             dtrain = xgb.QuantileDMatrix(features.get(), Y.get())
 
             evals = [(dtrain, 'train')]
@@ -67,14 +73,15 @@ class SDE:
                 dtrain,
                 num_boost_round=64,
                 evals=evals,
-                early_stopping_rounds=5
+                early_stopping_rounds=5,
+                verbose_eval=False
             )
 
-            # TODO: inplace predict supports cupy arrays on recent versions
+            # TODO: inplace predict supports CuPy arrays on newer versions of XGBoost
             dfeatures = xgb.DMatrix(features.get())
             C = cp.asarray(model.predict(dfeatures))
 
-            exercise_payoff = cp.maximum(1.0 - self.X[t, item], 0.0)
+            exercise_payoff = cp.maximum(self.strike - self.X[t, item], 0.0)
             exercise = exercise_payoff >= C
             V[item] = cp.where(exercise, exercise_payoff, V[item])
 
@@ -84,4 +91,4 @@ class SDE:
                 self.stopping_times[item]
             )
 
-        return cp.mean(V)
+        self.optimal_stopping = cp.mean(V)
